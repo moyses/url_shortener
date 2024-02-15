4d2824638c4485cb217f2c69ccbdf37fc0849311
Moyses Viana <moysesspj@gmail.com>
2024-02-15 04:01:28 -0300

    improves link model controle create a validade form and improves view links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,21 @@
 class LinksController < ApplicationController
   def index
-    @links = Link.all
+    @links = Link.recent_first
+  end
+
+  def create
+    @links = Link.new(links_params)
+    if @links.save
+      redirect_to root_path
+    else
+      index
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def links_params
+    params.require(:link).permit(:url)
   end
 end
